cae2fff9f627e8c1199d16dac11b43325f6f2feb
Pickle String <picklestring@gmail.com>
2025-05-12 22:27:10 +1000

    basic

---
+++ TestingSuite/Program.cs
@@ -0,0 +1,10 @@
+﻿using SimuliteCSharp;
+using SimuliteCSharp.Nodes;
+using SimuliteCSharp.Values;
+INode program = SimuliteApi.Parse(File.ReadAllText("./test.sim"));
+SimuliteEnvironment env = new();
+env.AddVariable("print", new RuntimeExternalFunction((IRuntimeValue[] args) =>
+{
+	Console.WriteLine(args[0].Print());
+}));
+program.Evaluate(env);
