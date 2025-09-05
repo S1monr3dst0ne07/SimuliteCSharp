91be1f00667fc0f0d35004f88b7382ea41b9bc69
S1mon <jaforsi.kozak@gmail.com>
2025-09-05 06:00:33 +0200

    refactored FunctionCallNode

--- SimuliteCSharp/Nodes/FunctionCallNode.cs
+++ SimuliteCSharp/Nodes/FunctionCallNode.cs
@@ -3,25 +3,28 @@ namespace SimuliteCSharp.Nodes;
 
 public class FunctionCallNode(string identifier, INode?[] parms) : INode
 {
+	//get parameter values for call
+	private IRuntimeValue?[] GetParamValues(SimuliteEnvironment env)
+	{
+		return parms.Select(
+			param => param?.Evaluate(env)
+		).ToArray();
+	}
+
+
 	public IRuntimeValue? Evaluate(SimuliteEnvironment env)
 	{
-		IRuntimeValue value = env.ResolveLocal(identifier);
-		switch (value)
+		switch (env.ResolveLocal(identifier))
 		{
 		case RuntimeExternalFunction exFunc:
-			exFunc.Method.Invoke(parms.Select(line => line.Evaluate(env)).ToArray());
+            exFunc.Method.Invoke(GetParamValues(env));
 			return null;
 		case RuntimeFunction func:
 			SimuliteEnvironment funcEnv = new SimuliteEnvironment(func.ParentEnv);
-			IRuntimeValue?[] parsedParams = parms.Select(line => line.Evaluate(env)).ToArray();
-			string[] functionDefParams = func.ParamList;
-			Dictionary<string, IRuntimeValue?> paramMap = functionDefParams
-				.Zip(parsedParams, (first, second) => new {first, second})
-				.ToDictionary(val => val.first, val => val.second);
-			foreach (KeyValuePair<string, IRuntimeValue?> param in paramMap)
-			{
-				funcEnv.AddLocal(param.Key, param.Value);
-			}
+
+			foreach (var (key, value) in func.ParamList.Zip(GetParamValues(env)))
+				funcEnv.AddLocal(key, value);
+
 			return func.Block.Evaluate(funcEnv);
 		default:
 			throw new Exception("Invalid function call, calling a variable is not a valid statement.");
