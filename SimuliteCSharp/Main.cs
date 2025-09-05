7c4706be2c4237e5a0abc27149aee7767488b24c
S1mon <jaforsi.kozak@gmail.com>
2025-09-05 03:00:41 +0200

    implemented entry point

---
+++ SimuliteCSharp/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NDesk.Options;
+using SimuliteCSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var path = "Program.txt";
+        var p = new OptionSet()
+        {
+            { "i|input=", v => path = v }
+        };
+        p.Parse(args);
+
+        var source = File.ReadAllText(path);
+        var ast = SimuliteApi.Parse(source);
+        var env = new SimuliteEnvironment();
+
+        ast.Evaluate(env);
+
+    }
+}
